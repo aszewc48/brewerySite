f5b75de2a8a670809980e6ca54ac01ffae184d1d
Adam Szewczyk <aszewczyk1990@hotmail.com>
2022-09-16 17:18:40 -0400

    removed country from USA

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -9,18 +9,32 @@ router.get("/", (req, res, next) => {
 
 router.get('/getAllBrew', (req,res,next) => {
   axios.get(`https://api.openbrewerydb.org/breweries?per_page=50`)
-  .then(response => {
-    console.log('Brewery data:', response)
-    res.render('allBrews.hbs', { breweries: response.data})
+  .then(libData => {
+    console.log('Data for searching library:', libData)
+    let noUSAArray = libData.data.map(element => {
+      const elementCopy = {...element};
+      if(elementCopy.country.toLowerCase() === 'united states'){
+        delete elementCopy.country;
+      }
+      return elementCopy;
+    })
+    res.render('BreweryLibrary.hbs', {breweries: noUSAArray})
   })
   .catch(err => console.log('Error getting brewery:', err))
 })
 
 router.get('/getRandomBrew', (req,res,next) => {
   axios.get(`https://api.openbrewerydb.org/breweries/random`)
-  .then(ranResponse => {
-    console.log('Random brewery data:', ranResponse.data)
-    res.render('randomBrew.hbs', { ranBreweries: ranResponse.data[0]})
+  .then(libData => {
+    console.log('Data for searching library:', libData)
+    let noUSAArray = libData.data.map(element => {
+      const elementCopy = {...element};
+      if(elementCopy.country.toLowerCase() === 'united states'){
+        delete elementCopy.country;
+      }
+      return elementCopy;
+    })
+    res.render('BreweryLibrary.hbs', {breweries: noUSAArray})
   })
   .catch(err => console.log('Error getting random brewery:', err))
 
@@ -30,7 +44,14 @@ router.get('/brewLibrary/:name', (req,res,next) => {
   axios.get(`https://api.openbrewerydb.org/breweries?by_name=${req.params.name}&per_page=50`)
   .then(libData => {
     console.log('Data for searching library:', libData)
-    res.render('BreweryLibrary.hbs', {breweries: libData.data})
+    let noUSAArray = libData.data.map(element => {
+      const elementCopy = {...element};
+      if(elementCopy.country.toLowerCase() === 'united states'){
+        delete elementCopy.country;
+      }
+      return elementCopy;
+    })
+    res.render('BreweryLibrary.hbs', {breweries: noUSAArray})
   })
   .catch(err => console.log('Error looking through library:', err))
 })
